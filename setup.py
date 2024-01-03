3ca140ca0ecf079b07d67999af899595f0ab3d6f
Sandeep <sandeepiit.ism@gmail.com>
2024-01-03 22:58:02 +0530

    mongo_crud and setup.cfg updated

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "mongodbmlops"
+PKG_NAME= "mongodbdatabase"
+AUTHOR_USER_NAME = "sandeep chowdhury"
+AUTHOR_EMAIL = "sandeepiit.ism@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with mongodb database and mlops.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
